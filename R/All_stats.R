8febaa56def1512164914fff039a5a3577adb61e
Daehwan Kim <87584921+daehwankim12@users.noreply.github.com>
2023-10-20 13:14:37 +0900

    Bug fix

--- R/All_stats.R
+++ R/All_stats.R
@@ -40,7 +40,7 @@ All_stats <-
 
     # Convert the data frame to a data table
     Data_final <- data.table::as.data.table(Data)
-    Data_final_raw <- Data_final[, !c("Sample", "Group"), with = FALSE]
+    Data_final_raw <- Data_final[, setdiff(names(Data_final), c("Sample", "Group")), with = FALSE]
 
     # Convert the "Group" column to a factor
     Data_final[, Group := as.factor(as.character(Group))]
